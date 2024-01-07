d9bec345a46a58291632690cf5e7c73453a7d4ae
Jay/Fienna Liang <roackb2@gmail.com>
2024-01-07 17:03:54 +0800

    REPL with .exit command

--- cmd/repl/main.go
+++ cmd/repl/main.go
@@ -1,12 +1,31 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
-
-	buffer_manager "github.com/roackb2/simple_db/internal/buffer"
+	"os"
+	// buffer_manager "github.com/roackb2/simple_db/internal/buffer"
 )
 
+func print_usage() {
+	fmt.Println("Simple DB 0.0.1")
+	fmt.Println("Type .exit to exit")
+}
+
+func print_prompt() {
+	fmt.Print("db > ")
+}
+
 func main() {
-	fmt.Println("Running REPL")
-	buffer_manager.Allocate()
+	// buffer_manager.Allocate()
+	print_usage()
+	for {
+		print_prompt()
+		reader := bufio.NewReader(os.Stdin)
+		text, _ := reader.ReadString('\n')
+		if text == ".exit\n" {
+			break
+		}
+		fmt.Println(text)
+	}
 }
